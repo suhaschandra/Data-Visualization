13287c38cf022f68e7e084e977dc980a5468545d
suhaschandra <ssureshc@uci.edu>
2018-05-17 12:31:06 -0700

    Update heatmap_rnaseq_unmerged.R
    Includes updates to merge gene names with RPKMs internally.

--- heatmap_rnaseq_unmerged.R
+++ heatmap_rnaseq_unmerged.R
@@ -8,16 +8,25 @@ library(lattice)
 #Define the colors
 hmcol <- colorRampPalette(brewer.pal(9, "RdBu"))(100)
 
-# file1 is the list of merged genes
+# file1 is the list of gene names with no header
 file1  <- args[1]
+# file2 is the mastersheet with RPKM values
+file2 <- args[2]
 # Output name - heatmap pdf prefix
-outprefix <- args[2]
+outprefix <- args[3]
 
 #Open file handles and save the first columns
-f1 <- read.delim(file1, sep="\t", header=TRUE, row.names=1)
+f1 <- read.delim(file1, sep="\t", header=FALSE, row.names=1)
+f2 <- read.delim(file2, sep="\t", header=TRUE, row.names=1)
+
+# Select column names for RPKM
+colnames = c("", "")
+merge <- merge(f1, f2, by.x="V1", by.y="GENE")
+merge_subset <- subset(merge, select = colnames)
+rownames(merge_subset) <- merge$V1
 
 ############################################################
-y <- t(scale(t(as.matrix(f1))))
+y <- t(scale(t(as.matrix(merge_subset))))
 
 #Printing the heatmap
 pdf(paste(outprefix, "_heatmap.pdf", sep=""))
@@ -26,5 +35,5 @@ dev.off()
 
 
 pdf(paste(outprefix, "_clustered_heatmap.pdf", sep=""))
-heatmap.2(as.matrix(f1), col=rev(hmcol), scale="row", key=T, keysize=1.5, density.info="density", cexRow=0.1, cexCol=0.9)
+heatmap.2(as.matrix(merge_subset), col=rev(hmcol), scale="row", key=T, keysize=1.5, density.info="density", cexRow=0.1, cexCol=0.9)
 dev.off()
