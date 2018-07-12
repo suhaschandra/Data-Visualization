b66a4b810ebf82c22d41960247c7672b8b7c23ca
suhaschandra <ssureshc@uci.edu>
2018-07-12 14:24:01 -0700

    Update heatmap_mapped.R

--- Heatmaps/heatmap_mapped.R
+++ Heatmaps/heatmap_mapped.R
@@ -1,1 +1,31 @@
 
+#Input one file  (which is the merged dataset).
+#Output - two heatmaps (clusters and unclustered)
+
+args <- commandArgs(TRUE)
+library("RColorBrewer")
+library(gplots)
+library(lattice)
+
+#Define the colors
+hmcol <- colorRampPalette(brewer.pal(9, "RdBu"))(100)
+
+# file1 is the list of merged genes
+inFile  <- args[1]
+# Output name - heatmap pdf prefix
+outprefix <- args[2]
+
+#Open file handles and save the first columns
+f <- read.delim(inFile, sep="\t", header=TRUE, row.names=1)
+
+############################################################
+y <- t(scale(t(as.matrix(f))))
+
+#Printing the heatmap
+pdf(paste(outprefix, "_heatmap.pdf", sep=""))
+levelplot(t(y), height=0.3, col.regions=rev(hmcol), main="", colorkey=list(space="top"), xlab="", ylab="", pretty=TRUE, width=0.5, cexRow=0.1, cexCol=0.1, aspect=2.5)
+dev.off()
+
+pdf(paste(outprefix, "_clustered_heatmap.pdf", sep=""))
+heatmap.2(as.matrix(f1), col=rev(hmcol), scale="row", key=T, keysize=1.5, density.info="density", cexRow=0.1, cexCol=0.9)
+dev.off()
